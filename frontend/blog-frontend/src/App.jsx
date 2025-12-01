e5a9dc6cfd3fb23ad1bc561fa58dd246393a435a
LearningMasterA <ankitasingh0263@gmail.com>
2025-12-02 00:29:43 +0530

    navbar changed

--- frontend/blog-frontend/src/App.jsx
+++ frontend/blog-frontend/src/App.jsx
@@ -5,23 +5,18 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import SinglePost from './pages/SinglePost'
 import Navbar from './components/Navbar';
-import CreatePost from './pages/CreatePost';
+// import CreatePost from './pages/CreatePost';
 
 function App(){
   return (
     <BrowserRouter future={{ v7_relativeSplatPath: true }}>
-      {/* <nav style={{ padding: "10px", background: "#f4f4f4" }}>
-        <a href="/" style={{ marginRight: "20px" }}>Home</a>
-        <a href="/login" style={{ marginRight: "20px" }}>Login</a>
-        <a href="/register">Register</a>
-      </nav> */}
       <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path="/post/:id" element={<SinglePost />} />
-      <Route path='/create' element={<CreatePost />}/>
+      {/* <Route path='/create' element={<CreatePost />}/> */}
 
     </Routes>
     </BrowserRouter>
--- frontend/blog-frontend/src/components/Navbar.jsx
+++ frontend/blog-frontend/src/components/Navbar.jsx
@@ -1,28 +1,64 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-      <Link className="navbar-brand" to="/">Blog App</Link>
+  const isLoggedIn=!!localStorage.getItem("token");
+  const handleLogout=()=>{
+    localStorage.removeItem("token");
+    window.location.href="/login";
+  };
 
-      <div className="ms-auto">
-        {localStorage.getItem("token") ? (
-          <>
-            <Link className="btn btn-success me-2" to="/create">Create Post</Link>
-            <button
-              className="btn btn-danger"
-              onClick={() => {
-                localStorage.removeItem("token");
-                window.location.href = "/login";
-              }}
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <Link className="btn btn-primary" to="/login">Login</Link>
-        )}
+  return(
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand fw-bold" to="/">
+          📝 BlogApp
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            {isLoggedIn && (
+              <li className="nav-link">
+                <Link className="nav-link" to="/create">Create Post</Link>
+              </li>
+            )}
+
+            {!isLoggedIn && (
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+              </li>
+            )}
+
+            {!isLoggedIn ? (
+              <li className="nav-item">
+                <Link className="nav-link btn btn-primary text-white px-3 ms-2" to="/login">
+                  Login
+                </Link>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <button className="btn btn-danger px-3 ms-2" onClick={handleLogout}>
+                  Logout
+                </button>
+              </li>
+            )}
+            </ul>
+            </div>
       </div>
+
     </nav>
-  );
+    </>
+  )
 }
